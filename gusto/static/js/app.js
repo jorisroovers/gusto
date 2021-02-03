1916295277d595bc455738fd02892e487d5d9627
Joris Roovers <joris.roovers@gmail.com>
2021-02-03 22:35:59 +0100

    Recipe selector + dependency bump

--- gusto/static/js/app.js
+++ gusto/static/js/app.js
@@ -41,6 +41,126 @@ var websocket = new Vue({
     }
 })
 
+Vue.component('meal-row-placeholder', {
+    props: ['meal'],
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: `
+    <tr class="placeholder">
+        <td class="no-wrap">{{ meal.date.format('dddd') }}</td>
+        <td class="no-wrap">{{ meal.date.format('YYYY-MM-DD') }}</td>
+        <td>No meal planned</td>
+        <td></td>
+        <td></td>
+    </tr>
+    `
+})
+Vue.component('meal-row-normal', {
+    props: ['meal'],
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: `
+    <tr>
+        <td class="no-wrap">{{ meal.date.format('dddd') }}</td>
+        <td class="no-wrap">{{ meal.date.format('YYYY-MM-DD') }}</td>
+        <td>
+            <a v-if="meal.recipe.url" :href="meal.recipe.url" target="_blank">
+                {{ meal.recipe.name }}
+            </a>
+            <template v-else>{{ meal.recipe.name }}</template>
+        </td>
+        <td class="no-wrap">{{ meal.constraint == null ? "NA" : meal.constraint.title }}</td>
+        <td class="no-wrap">
+            <span class="tag" :data-value="tag.name" v-for="tag in meal.recipe.tags">
+                {{tag.display_name}}
+            </span>
+        </td>
+    </tr>
+    `
+})
+
+Vue.component('recipe-selector', {
+    props: ['recipes'],
+    data: function () {
+        return { isActive: false, selectedRecipe: null }
+    },
+    computed: {
+        classObject: function () {
+            return { 'is-active': this.isActive }
+        }
+    },
+    methods: {
+        selectRecipe: function (recipe) {
+            this.selectedRecipe = recipe
+            this.isActive = false
+        }
+    },
+    template: `
+    <div class="dropdown" v-bind:class="classObject" >
+    <div class="dropdown-trigger" v-on:click="isActive=!isActive">
+        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+        <span v-if="selectedRecipe==null">Select a recipe</span>
+        <span v-else>{{selectedRecipe.name}}</span>
+        <span class="icon is-small">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </span>
+        </button>
+    </div>
+    <div class="dropdown-menu" id="dropdown-menu" role="menu">
+        <div class="dropdown-content">
+        <a href="#" class="dropdown-item" v-for="recipe in recipes" v-on:click="selectRecipe(recipe)">
+            {{ recipe.name }}
+        </a>
+        </div>
+    </div>
+</div>
+    `
+})
+
+
+
+Vue.component('meal-row-edit', {
+    props: ['meal'],
+    data: function () {
+        return { recipes: [] }
+    },
+    created() {
+        const self = this
+        axios.get('/api/recipes')
+            .then(function (response) {
+                self.recipes = response.data.recipes
+            })
+    },
+    template: `
+    <tr>
+        <td class="no-wrap">{{ meal.date.format('dddd') }}</td>
+        <td class="no-wrap">{{ meal.date.format('YYYY-MM-DD') }}</td>
+        <td><recipe-selector v-bind:recipes="recipes" /></td>
+        <td></td>
+        <td></td>
+    </tr>
+    `
+})
+
+
+Vue.component('meal-row', {
+    props: ['meal'],
+    computed: {
+        classObject: function () {
+            return { 'is-today': this.meal.date.isSame(new Date(), 'day') }
+        }
+    },
+    template: `
+        <meal-row-edit v-bind:class="classObject" v-bind:meal="meal" v-if="meal.placeholder" />
+        <meal-row-normal v-bind:class="classObject" v-bind:meal="meal" v-else />
+    `
+})
 
 var mealplan = new Vue({
     el: '#mealplan',
